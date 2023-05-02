63064918d5e95ecce83eed959a9b08c031f86725
joy4563 <joymondal4563@gmail.com>
2023-05-02 12:45:52 +0600

    load all chef and a single chef details

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const chefs = require("./chef.json");
+
 
 app.get("/", (req, res) => {
     res.send("chef is cooking...");
 });
 
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
+});
 
+app.get("/chefs/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const chef = chefs.filter((n) => parseInt(n.id) === id);
+
+    res.send(chef);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
